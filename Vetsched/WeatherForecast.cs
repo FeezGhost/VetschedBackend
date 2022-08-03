a5f172d29764815c58d3870ed73ae4421ccb6b28
Muhammad Usman <muhammad.usman@gagamuller.com>
2022-08-04 00:32:48 +0500

    initial commit

--- Vetsched/WeatherForecast.cs
+++ Vetsched/WeatherForecast.cs
@@ -5,7 +5,7 @@ namespace Vetsched
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
-
+        //first commit
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
