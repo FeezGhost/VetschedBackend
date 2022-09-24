fc6fe5d9b3592489a7cdb246ee8d7bec25c35f83
Muhammad Usman <muhammad.usman@gagamuller.com>
2022-09-25 04:16:10 +0500

    user profile end points added

---
+++ Vetsched/Controllers/UserProfileController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vetsched.Data.Dtos.Request;
+using Vetsched.Data.Dtos.Response;
+using Vetsched.Services;
+
+namespace Vetsched.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileService _service;
+        public UserProfileController(IUserProfileService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("Profiles")]
+        public async Task<ActionResult<List<UserBaseResponseDto>>> GetProfiles(Guid UserId)
+        {
+            var res = await _service.GetUserProfiles(UserId);
+            return Ok(res);
+        }
+        [HttpGet("Profile")]
+        public async Task<ActionResult<UserBaseResponseDto>> GetProfile(Guid ProfileId)
+        {
+            var res = await _service.GetUserProfile(ProfileId);
+            return Ok(res);
+        }
+        [HttpPost("Add")]
+        public async Task<ActionResult<UserBaseResponseDto>> CreateProfile(UserProfileCreateRequestDto request)
+        {
+            var res = await _service.CreateUserProfile(request);
+            return Ok(res);
+        }
+        [HttpPut("Update")]
+        public async Task<ActionResult<UserBaseResponseDto>> UpdateProfile(UserProfileUpdateRequestDto request)
+        {
+            var res = await _service.UpdateUserProfile(request);
+            return Ok(res);
+        }
+        [HttpDelete("Delete")]
+        public async Task<ActionResult<UserBaseResponseDto>> DeleteProfile(Guid ProfileId)
+        {
+            var res = await _service.DeleteUserProfile(ProfileId);
+            return Ok(res);
+        }
+    }
+}
